5a8eb63905b5c429bb3c2f5091682a15b9087f55
behzad00 <behzadpouyanfar@gmail.com>
2017-05-29 22:32:58 -0400

    Added app.get to html route

--- routes/html-api-routes.js
+++ routes/html-api-routes.js
@@ -1,10 +1,12 @@
 var users = require('./models/users.js');
+var express = require("express");
 var links = require('./models/links.js');
 var subject = require('./models/subject.js');
 var topic = require('./models/topic.js');
 var passport = require('passport');
 var Sequelize = require("sequelize");
 var sequelize = require("./config/connection.js");
+var express = require("express");
 
 
 function loggedIn(req, res, next) {
@@ -14,6 +16,11 @@ function loggedIn(req, res, next) {
 		res.redirect('/login');
 	}
 }
+
+app.get("/", function(req, res) {
+  // send us to the next get function instead.
+  res.redirect("/fields");
+});
 
 module.exports = function(app){
 	
