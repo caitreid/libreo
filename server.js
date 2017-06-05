b7a63cb2b45239378e06819b8b2aadbdfd66add1
C. Reid <ctlnrd@gmail.com>
2017-06-05 18:52:24 -0400

    dynamically generate topics in the pull-down for create-links
    success

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var session = require("express-session");
 
 // bring in the models
 var db = require("./models");
-
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
@@ -45,7 +44,6 @@ app.use("/signup",routes);
 app.use("/login",routes);
 app.use("/welcome",routes);
 
-
 // listen on port 3000
 var port = process.env.PORT || 3000;
 db.sequelize.sync().then(function() {
@@ -53,66 +51,10 @@ db.sequelize.sync().then(function() {
   console.log("app is listening on port: " + port)
 });
 
+// require("./routes/create-link-routes.js")(app);
 
 
 
-// // initial requires
-// var express = require('express');
-// var bodyParser = require('body-parser');
-
-// // setup the express app
-// var app = express();
-
-// // use body-parser to help express handle requests
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({type:'application/vnd.api+json'}));
-
-// // set up handlebars for express
-// var exphbs = require('express-handlebars');
-// app.engine('handlebars', exphbs({
-//   defaultLayout: 'main',
-//     extname: '.handlebars',
-//     layoutsDir: 'app/views/layouts'
-// }));
-// app.set('view engine', 'handlebars');
-// app.set('views', __dirname + '/app/views');
-
-// // load the static files
-// var staticContentFolder = __dirname + '/app/public';
-// app.use(express.static(staticContentFolder));
-
-
-// // require passport.js
-// // require("./app/config/passport.js")(app)
-
-// // express sitemap app
-// // var sitemap = require('express-sitemap');
-// // var map = sitemap({
-// //     generate: app
-// // });
-
-// // app.get('/sitemap.xml', function(req, res) { // send XML map
-// //     map.XMLtoWeb(res);
-// // }).get('/robots.txt', function(req, res) { // send TXT map
-// //     map.TXTtoWeb(res);
-// // });
-
-// // require the api and html paths
-// require("./routes/subject-api-routes.js")(app)
-// require("./routes/topic-api-routes.js")(app)
-// require("./routes/links-api-routes.js")(app)
-// require("./routes/html-api-routes.js")(app)
-// // require all of the database connections
-// // require("./app/models/db_relations.js")(app)
-
-
-// // start the server
-// var PORT = process.env.PORT || 8080;
-// app.listen(PORT, function(){
-// 	console.log('App is listening: ' + PORT);
-// });
 
 
 
